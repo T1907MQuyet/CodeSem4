731d54c30ffec695d9e7407f0873dbf93eb47db8
T1907MQuyet <53265406+T1907MQuyet@users.noreply.github.com>
2021-05-07 11:09:53 +0700

    upcode login re

--- springboot-registration-login/src/main/java/com/example/auth/repository/UserCrudRepository.java
+++ springboot-registration-login/src/main/java/com/example/auth/repository/UserCrudRepository.java
@@ -4,5 +4,6 @@ import com.example.auth.model.User;
 import org.springframework.data.repository.CrudRepository;
 
 public interface UserCrudRepository extends CrudRepository<User,Long> {
-    
+
+
 }
